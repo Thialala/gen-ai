cc729719b92712c78f3b14bb59d16b375d5bca5f
Ousmane Barry <ousmane.barry@wineos-group.com>
2024-07-17 22:23:56 +0200

    Mise à jour majeure et ajout de fonctionnalités
    Mise à jour des dépendances, notamment `Azure.Identity` de `1.10.4` à `1.12.0` et `Microsoft.SemanticKernel` de `1.0.1` à `1.16.0`. Ajout de `Microsoft.Data.SqlClient` v`5.2.1`, `Microsoft.SemanticKernel.Plugins.Core` v`1.14.1-alpha`, `Microsoft.SemanticKernel.Plugins.Memory` v`1.14.1-alpha`, et `Spectre.Console` v`0.49.1` pour renforcer les capacités du projet.
    
    Introduction d'un nouveau plugin SQL dans `SQLPlugin.cs` offrant des fonctionnalités avancées de gestion de base de données SQL, y compris l'exécution de requêtes et la récupération de schémas de table au format CSV.
    
    Modifications significatives dans `Program.cs` : suppression de `Azure.Identity` au profit de `FunctionCalling.Plugins`, ajout de configurations pour une clé API OpenAI et une chaîne de connexion SQL, transition vers `AddOpenAIChatCompletion` avec GPT-4o, intégration du plugin SQL, ajustement de l'UI de la console de bleu à blanc, et modification de la méthode d'affichage des messages.
    
    Ces changements marquent une évolution importante dans la gestion des dépendances et l'intégration de nouvelles fonctionnalités pour une interaction plus riche avec les bases de données SQL et une amélioration de l'interface utilisateur.

---
+++ src/GenAISamples/FunctionCalling/Plugins/SQLPlugin.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.Data.SqlClient;
+using Microsoft.SemanticKernel;
+using System.Threading.Tasks;
+namespace FunctionCalling.Plugins
+{
+    public class SQLPlugin
+    {
+        private readonly string _connectionString;
+
+        public SQLPlugin(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+        [KernelFunction, Description("Obtain the table names in SunuElection, which contains Candidats, Bureaux de Vote, PVs and Elections results. Always run this before running other queries instead of assuming the user mentioned the correct name.")]
+        public string GetTables()
+        {
+            Console.WriteLine($"Getting tables...");
+            return QueryAsCSV($"SELECT TABLE_SCHEMA, TABLE_NAME FROM INFORMATION_SCHEMA.TABLES;");
+        }
+
+
+        [KernelFunction, Description("Obtain the database schema for a table in SunuElection.")]
+        public string GetSchema(
+            [Description("The table to get the schema for. Do not include the schema name.")] string tableName
+        )
+        {
+            Console.WriteLine($"Getting schema for table \"{tableName}\"...");
+            return QueryAsCSV($"SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{tableName}';");
+        }
+
+
+
+        [KernelFunction, Description("Run SQL against the SunuElection database")]
+        public string RunQuery(
+            [Description("The query to run on SQL Server. When referencing tables, make sure to add the schema names.")] string query
+        )
+        {
+            Console.WriteLine($"Running query...");
+            return QueryAsCSV(query);
+        }
+
+        private string QueryAsCSV(string query)
+        {
+            var output = "[DATABASE RESULTS] \n";
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                try
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        output += reader.GetName(i);
+                        if (i < reader.FieldCount - 1)
+                            output += ",";
+                    }
+                    output += "\n";
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            var columnName = reader.GetName(i);
+                            output += reader[columnName].ToString();
+                            if (i < reader.FieldCount - 1)
+                                output += ",";
+                        }
+                        output += "\n";
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    reader.Close();
+                }
+            }
+            return output;
+        }
+    }
+}
--- src/GenAISamples/FunctionCalling/Program.cs
+++ src/GenAISamples/FunctionCalling/Program.cs
@@ -1,11 +1,11 @@
-﻿using Azure.Identity;
-using FunctionCalling.Plugins;
+﻿using FunctionCalling.Plugins;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Core;
 
 // Before running this sample, you must add the following secrets to the project via SecretManager:
 // - AzureOpenAI:DeploymentName
@@ -25,18 +25,26 @@ var deploymentName = configuration["AzureOpenAI:DeploymentName"];
 var endpoint = configuration["AzureOpenAI:Endpoint"];
 var bingSubscriptionKey = configuration["BingCustomSearch:SubscriptionKey"];
 var bingCustomConfigId = configuration["BingCustomSearch:CustomConfigId"];
+var openAIKey = configuration["OpenAI:ApiKey"];
+var sqlConnectionString = configuration["SQL:ConnectionString"];
 
 // Create the kernel and configure it
 var builder = Kernel.CreateBuilder();
-builder.AddAzureOpenAIChatCompletion(deploymentName, endpoint, new DefaultAzureCredential());
+//builder.AddAzureOpenAIChatCompletion(deploymentName, endpoint, new DefaultAzureCredential());
+builder.AddOpenAIChatCompletion("gpt-4o", openAIKey);
 builder.Services.AddLogging(c => c.AddDebug().SetMinimumLevel(LogLevel.Trace));
 var kernel = builder.Build();
 
 // Add the Bing Custom Search plugin
 kernel.ImportPluginFromObject(new WebSearchEnginePlugin(bingSubscriptionKey, bingCustomConfigId), "bing");
+// Add the Leave plugin
 kernel.ImportPluginFromObject(new LeavePlugin(), "leave");
+// Add the SQL plugin
+kernel.ImportPluginFromObject(new SQLPlugin(sqlConnectionString), "sql");
+#pragma warning disable SKEXP0050
+kernel.ImportPluginFromType<TimePlugin>();
+#pragma warning restore SKEXP0050 
 
-// Create chat history
 ChatHistory history = [];
 
 // Get chat completion service
@@ -63,7 +71,7 @@ while (true)
         kernel: kernel);
 
 
-    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.ForegroundColor = ConsoleColor.White;
     Console.WriteLine();
 
     // Stream the results
@@ -79,6 +87,7 @@ while (true)
         Console.Write(content.Content);
         fullMessage += content.Content;
     }
+    //AnsiConsole.MarkupLine(fullMessage);
     Console.WriteLine();
     Console.WriteLine();
 
